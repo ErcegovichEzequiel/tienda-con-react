865d4059d9946e4d4f1a99a06a8b2010d32cfcd1
Ezequiel Ercegovich <ercegovich.ezequiel@gmail.com>
2024-02-18 17:30:54 -0300

    iniciarSesion

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FaCartShopping } from "react-icons/fa6";
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 
 
@@ -12,11 +12,11 @@ function Banner() {
     return (
         <>
             <Navbar className='navSup'>
-                    <Nav className="navListaGeneral">
-                        <Link className='navLista' to="/src/Components/IniSesion/IniciaSesion.jsx">Inisio de Sesión</Link>
-                        <Link className='navLista' to="/src/Components/Registrarme">Regístrate</Link>
-                        <Link className='navLista' to="/src/Components/Carrito/Carrito.jsx"><FaCartShopping /></Link>
-                    </Nav>
+                <Nav className="navListaGeneral">
+                    <Link className='navLista' to="/src/Components/IniSesion/IniciaSesion.jsx">Inisio de Sesión</Link>
+                    <Link className='navLista' to="/src/Components/Registrarme">Regístrate</Link>
+                    <Link className='navLista' to="/src/Components/Carrito/Carrito.jsx"><FaCartShopping /></Link>
+                </Nav>
             </Navbar>
 
             <section className='banner'>
@@ -29,17 +29,15 @@ function Banner() {
             </section>
 
             <Navbar expand="lg" className="navInf">
-                    <Navbar.Toggle className="listaInferior" aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse className="listaInferior">
-                        <Nav className="listaGeneralInferior">
-                            <Nav.Link> <Link className="listaInferior" to="/">Inicio</Link></Nav.Link>
-                            <Nav.Link> <Link className="listaInferior" to="/">Contactanos</Link></Nav.Link>
-                            <NavDropdown  title="Tienda" >
-                                <NavDropdown.Item><Link className="listaInferiorDesplegable" to="/src/Components/Tienda/Tienda.jsx">Juegos</Link></NavDropdown.Item>
-                                <NavDropdown.Item><Link className="listaInferiorDesplegable" to="/src/Components/Banner/EnConstruccion.jsx">Consolas</Link></NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
+                <Navbar.Toggle className="listaInferior" aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="listaInferior">
+                    <Nav className="listaGeneralInferior">
+                        <Nav.Link> <Link className="listaInferior" to="/">Inicio</Link></Nav.Link>
+                        <Nav.Link> <Link className="listaInferior" to="/">Contactanos</Link></Nav.Link>
+                        <Nav.Link><Link className="listaInferior" to="/src/Components/Tienda/Tienda.jsx">Juegos</Link></Nav.Link>
+                        <Nav.Link><Link className="listaInferior" to="/src/Components/Banner/EnConstruccion.jsx">Consolas</Link></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         </>
     )
--- src/Components/IniSesion/IniciaSesion.jsx
+++ src/Components/IniSesion/IniciaSesion.jsx
@@ -2,8 +2,9 @@
 import { FaUserLock } from "react-icons/fa"
 import { RiLockPasswordFill } from "react-icons/ri"
 import { useState } from "react"
+import { Link } from 'react-router-dom';
 
-export const IniciaSesion = () =>{
+export const IniciaSesion = () => {
     const [userInput, setUserInput] = useState("")
     const [passwordInput, setPasswordInput] = useState("")
     const handleButtonClick = (event) => {
@@ -15,35 +16,40 @@ export const IniciaSesion = () =>{
             alert("Por favor, complete los campos de usuario y contraseña.")
         }
     }
-    const handleUserInputChange = (event) =>{
+    const handleUserInputChange = (event) => {
         setUserInput(event.target.value)
     }
-    const handlePasswordInputChange = (event) =>{
+    const handlePasswordInputChange = (event) => {
         setPasswordInput(event.target.value)
     }
     return (
-            <section className="formulario">
-                <form className="cform">
-                    <h3>Inicio de sesión</h3>
-                    <div className="bartex">
-                        <input className="bartexInput" type="text" placeHolder="Usuario" required value={userInput} onChange={handleUserInputChange} />
-                        <p className="iconinpu"><FaUserLock /></p>
-                    </div>
-                    <div className="bartex">
-                        <input className="bartexInput" type="password" placeHolder="Contraseña" required value={passwordInput} onChange={handlePasswordInputChange} />
-                        <p className="iconinpu"><RiLockPasswordFill /></p>
-                        
-                    </div>
-                    <div className="recordarme">
-                        <input type="checkbox" className="check" />
-                        <p>Recordarme</p>
-                        <p className="omc">Olvide mi contraseña</p>
-                    </div>
-                    <button type="submit" className="btn" onClick={handleButtonClick}>Ingresar</button>
-                    <br />
-                    <p>No tiene cuenta? <a href="registro.html">Registrate</a></p>
-                </form>
-            </section>
+        <section className="formulario">
+            <form className="cform">
+                <h3>Inicio de sesión</h3>
+                <div className="bartex">
+                    <input className="bartexInput" type="text" placeHolder="Usuario" required value={userInput} onChange={handleUserInputChange} />
+                    <p className="iconinpu"><FaUserLock /></p>
+                </div>
+                <div className="bartex">
+                    <input className="bartexInput" type="password" placeHolder="Contraseña" required value={passwordInput} onChange={handlePasswordInputChange} />
+                    <p className="iconinpu"><RiLockPasswordFill /></p>
+
+                </div>
+                <div className="recordarme">
+                    <input type="checkbox" className="check" />
+                    <p className="recordarmeP">Recordarme</p>
+
+                </div>
+                <Link to="/" className="omc">Olvide mi contraseña</Link>
+                <button type="submit" className="btn" onClick={handleButtonClick}>Ingresar</button>
+                <br />
+                <div className="registrameInicioSesion">
+                    <p>No tiene cuenta?</p> <Link className="linkRecordarme" to="/src/Components/Registrarme">Registrate</Link>
+                </div>
+
+
+            </form>
+        </section>
 
     )
 }
